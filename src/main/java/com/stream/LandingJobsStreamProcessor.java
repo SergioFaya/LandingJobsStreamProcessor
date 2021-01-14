5b62652adc8dbc4aaa94ba7f862ffca0f37806cf
Sergio Faya <sergiofayafernandez@gmail.com>
2021-01-14 18:45:21 +0100

    master: created development docker-compose and updated to use application.properties config

--- src/main/java/com/stream/LandingJobsStreamProcessor.java
+++ src/main/java/com/stream/LandingJobsStreamProcessor.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 @SpringBootApplication
-public class StreamProcessorApplication {
+@EnableKafkaStreams
+public class LandingJobsStreamProcessor {
     public static void main(String[] args) {
-        SpringApplication.run(StreamProcessorApplication.class, args);
+        SpringApplication.run(LandingJobsStreamProcessor.class, args);
     }
 }
 
